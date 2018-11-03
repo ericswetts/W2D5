b023c25ed405b2b3f803f6cfd29caa4d549b33b0
ericswetts <eric.swetts@gmail.com>
2018-11-02 17:06:03 -0700

    Complete ::Array and ::String hash methods

--- LRU_cache/lib/p02_hashing.rb
+++ LRU_cache/lib/p02_hashing.rb
@@ -1,21 +1,38 @@
 class Fixnum
-  # Fixnum#hash already implemented for you
+  # Fixnum#hash already implemented for you 
 end
 
 class Array
-  def hash
+    def hash
+      result = ""
+        self.each do |el|
+          result += el.to_s(2)
+          result += "_"
+        end
+      result.to_i
   end
 end
 
 class String
   def hash
+    alphabet = ([*('A'..'Z')].concat(('a'..'z').to_a)).rotate(14) # :*)
+    answer = ""
+    self.chars.each do |ch|
+      answer += (alphabet.index(ch) + 3).to_s(2)
+    end
+    answer.to_i
   end
 end
 
-class Hash
-  # This returns 0 because rspec will break if it returns nil
-  # Make sure to implement an actual Hash#hash method
-  def hash
-    0
-  end
-end
+# class Hash
+#   # This returns 0 because rspec will break if it returns nil
+#   # Make sure to implement an actual Hash#hash method
+#   def hash
+#     answer = ""
+# 
+#     self.to_a.sort.each do |subel|
+#       case subel.is_a?
+#     end
+#     0
+#   end
+# end
